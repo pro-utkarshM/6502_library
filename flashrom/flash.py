e346112811a30b58e8034a9ef0f78c08fcfb8506
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2025-04-18 14:14:02 +0300

    adding: flashrom config

--- flashrom/flash.py
+++ flashrom/flash.py
@@ -1,18 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
-# Pin maps
-ADDR_PINS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]   # A0–A14
-DATA_PINS = [17, 18, 19, 20, 21, 22, 23, 24]                       # I/O0–I/O7
+ADDR_PINS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]  # A0-A14
+DATA_PINS = [17, 18, 19, 20, 21, 22, 23, 24]                      # D0-D7
 CE = 25
 OE = 26
 WE = 27
 
-WRITE_DELAY = 0.001  # Delay after write (ms)
+WRITE_DELAY = 0.001  # Delay for EEPROM write (in seconds)
 
 def setup():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
     for pin in ADDR_PINS + DATA_PINS + [CE, OE, WE]:
         GPIO.setup(pin, GPIO.OUT)
     GPIO.output(CE, 1)
@@ -33,21 +31,20 @@ def write_byte(addr, data):
     GPIO.output(CE, 0)
     GPIO.output(OE, 1)
     GPIO.output(WE, 0)
-    time.sleep(0.00001)  # 10 us
+    time.sleep(0.00001)
     GPIO.output(WE, 1)
     GPIO.output(CE, 1)
     time.sleep(WRITE_DELAY)
 
-def flash_file(filename):
+def flash_rom(filename):
     with open(filename, "rb") as f:
-        contents = f.read()
-        size = len(contents)
-        print(f"[+] Flashing {size} bytes...")
-        for addr, byte in enumerate(contents):
-            write_byte(addr, byte)
-            if addr % 512 == 0:
-                print(f"[{addr:05X}] Writing 0x{byte:02X}")
-        print("[✓] Flash complete")
+        data = f.read()
+        print(f"[+] Flashing {len(data)} bytes")
+        for i, byte in enumerate(data):
+            write_byte(i, byte)
+            if i % 512 == 0:
+                print(f"[{i:04X}] → 0x{byte:02X}")
+        print("[✓] Done flashing")
 
 def cleanup():
     GPIO.cleanup()
@@ -55,6 +52,6 @@ def cleanup():
 if __name__ == "__main__":
     try:
         setup()
-        flash_file("firmware.bin")
+        flash_rom("rom.bin")
     finally:
         cleanup()
